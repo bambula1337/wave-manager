426ceaf4bf674540118d59066ded2e41007eced2
Oleg Golevitch <oleg133766@gmail.com>
2022-10-16 13:36:52 +0300

    :construction: Configured Eslint and Prettier
    Configred Eslint and Prettier configuration files

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,16 +3,15 @@ module.exports = {
   env: {
     node: true,
   },
-  extends: [
-    'plugin:vue/essential',
-    '@vue/airbnb',
-    '@vue/typescript/recommended',
-  ],
+  extends: ['plugin:vue/essential', '@vue/airbnb', '@vue/typescript/recommended'],
   parserOptions: {
     ecmaVersion: 2020,
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    '@typescript-eslint/no-inferrable-types': 'off',
+    'max-len': ['error', { code: 9999 }],
+    'global-require': 0,
   },
 };
