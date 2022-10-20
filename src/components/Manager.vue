3edb49854222077c80fbe17cf5eae7e6a5695586
Oleg Golevitch <oleg133766@gmail.com>
2022-10-20 15:23:47 +0300

    :dizzy: Added Animation to Manager Component
    Added Animation to Manager Component + added some features - now list based on data, you can add goals and delete list.

--- src/components/Manager.vue
+++ src/components/Manager.vue
@@ -1,50 +1,33 @@
 <template>
   <div class="manager">
-    <div class="list-wrapper">
-      <label for="name">
-        <textarea @input="heightFixer($event.target)" name="" id="name" class="name"></textarea>
-      </label>
-      <div class="list">
-        <label for="goal">
-          <textarea @input="heightFixer($event.target)" name="" id="goal" class="goal"
-                    :readonly="false"></textarea>
-        </label>
-      </div>
-    </div>
-    <div class="list-wrapper">
-      <div class="name-wrapper">
-        <label for="name">
-          <textarea @input="heightFixer($event.target)" name="" id="name" class="name"></textarea>
-        </label>
-        <div class="delete-wrapper">
-          <img src="@/assets/icons/cross.svg" alt="" class="icon">
-        </div>
-      </div>
-      <div class="list">
-        <div class="goal-wrapper">
-          <label for="goal">
-          <textarea @input="heightFixer($event.target)" name="" id="goal" class="goal"
-                    :readonly="false"></textarea><div class="delete-wrapper">
+    <transition-group name="lists-animation" class="transition-wrapper" tag="div">
+      <div class="list-wrapper" v-for="list in lists" :key="list.id">
+        <div class="list">
+          <div class="name-wrapper">
+            <label for="name">
+          <textarea @input="heightFixer($event.target)" name="" id="name" class="name"
+                    placeholder="Name" v-model="list.name"></textarea>
+            </label>
+            <div class="delete-wrapper" @click="deleteList(lists, list.id-1)"
+                 @keydown.ctrl="deleteList(lists, list.id)">
               <img src="@/assets/icons/cross.svg" alt="" class="icon">
             </div>
-          </label>
-        </div>
-        <div class="add-wrapper">
-          <img src="@/assets/icons/cross.svg" alt="" class="icon">
-        </div>
-      </div>
-    </div>
-    <div class="list-wrapper">
-      <label for="name">
-        <textarea @input="heightFixer($event.target)" name="" id="name" class="name"></textarea>
-      </label>
-      <div class="list">
-        <label for="goal">
+          </div>
+          <div class="list">
+            <div class="goal-wrapper">
+              <label for="goal" v-for="goal in list.goals" :key="goal.id">
           <textarea @input="heightFixer($event.target)" name="" id="goal" class="goal"
-                    :readonly="false"></textarea>
-        </label>
+                    :readonly="false" placeholder="Text" v-model="goal.text"></textarea>
+              </label>
+            </div>
+            <div class="add-wrapper" @click="createGoal(list.goals)"
+                 @keydown.enter="createGoal(list.goals)">
+              <img src="@/assets/icons/cross.svg" alt="" class="icon">
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
+    </transition-group>
   </div>
 </template>
 
@@ -53,10 +36,57 @@ import Vue from 'vue';
 
 export default Vue.extend({
   name: 'ManagerComponent',
+  data() {
+    return {
+      lists: [
+        {
+          id: 1,
+          name: 'Some List',
+          goals: [
+            {
+              id: 1,
+              text: 'Some Goal',
+            },
+          ],
+        },
+        {
+          id: 2,
+          name: 'Some',
+          goals: [
+            {
+              id: 1,
+              text: 'Some Goal',
+            },
+          ],
+        },
+        {
+          id: 3,
+          name: '',
+          goals: [
+            {
+              id: 1,
+              text: '',
+            },
+          ],
+        },
+      ],
+    };
+  },
   methods: {
     heightFixer(el: HTMLInputElement): void {
       el.style.height = '0px';
       el.style.height = `${el.scrollHeight}px`;
+    },
+    createGoal(list: object[]): void {
+      list.push(
+        {
+          id: list.length + 1,
+          text: '',
+        },
+      );
+    },
+    deleteList(list: never[], id: number): void {
+      list.splice(id, 1);
     },
   },
 });
@@ -65,32 +95,66 @@ export default Vue.extend({
 <style lang="scss" scoped>
 .manager {
   @apply flex flex-wrap text-project-paragraph font-project-default font-semibold mb-40 px-30 mt-20;
-  & .list-wrapper {
-    @apply w-%50 relative flex flex-col flex-grow border-4 border-project-stroke;
-    & .name {
-      @apply w-full h-18 text-center bg-project-background outline-none resize-none text-2xl border-b-4 border-project-stroke overflow-hidden px-26 py-5;
-    }
+  .transition-wrapper {
+    @apply flex flex-wrap w-full h-full;
+    & .list-wrapper {
+      @apply min-h-50 w-%50 flex flex-col flex-grow border-4 border-project-stroke;
+      & .list {
+        @apply w-full relative bg-project-background;
 
-    & .delete-wrapper {
-      @apply absolute top-5 right-4;
-      & .icon {
-        @apply w-8 opacity-50;
-      }
-    }
+        & .name {
+          @apply w-full h-18 text-center bg-project-background outline-none resize-none text-2xl border-b-4 border-project-stroke overflow-hidden px-26 py-5;
+        }
 
-    & .list {
-      @apply flex flex-col pl-10 my-5;
-      & .goal {
-        @apply w-full min-h-14 bg-project-background outline-none resize-none text-xl pr-22;
-      }
+        & .delete-wrapper {
+          @apply absolute top-5 right-4 cursor-pointer;
+          & .icon {
+            @apply w-8 opacity-50;
+          }
+        }
 
-      & .add-wrapper {
-        @apply self-end mr-5;
-        & .icon {
-          @apply w-9 rotate-45;
+        & .list {
+          @apply flex flex-col h-%50 pl-10 my-5 mb-15;
+
+          & .goal {
+            @apply w-full bg-project-background outline-none resize-none text-xl pr-22 mb-5 h-7;
+
+          }
+
+          & .add-wrapper {
+            @apply absolute -top-3 right-2 cursor-pointer;
+            & .icon {
+              @apply w-9 rotate-45;
+            }
+          }
         }
       }
     }
   }
+}
+
+//.lists-animation-move{
+//  transition: transform 1s !important;
+//}
+.lists-animation-move {
+  transition: all 0.7s;
+
+}
+
+/* apply transition to moving elements */
+.lists-animation-enter-active,
+.lists-animation-leave-active {
+  transition: all 1s;
+}
+
+.lists-animation-enter-from,
+.lists-animation-leave-to {
+  opacity: 0;
+}
+
+/* ensure leaving items are taken out of layout flow so that moving
+   animations can be calculated correctly. */
+.lists-animation-leave-active {
+  position: absolute;
 }
 </style>
